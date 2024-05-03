42e611e2bd6a5de84812b7226f141e57f93f162b
aayushjuhu <89008608+aayushjuhu@users.noreply.github.com>
2024-05-03 17:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from feature import FeatureExtraction
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from sklearn import metrics
 import warnings
 import pickle
 warnings.filterwarnings('ignore')
